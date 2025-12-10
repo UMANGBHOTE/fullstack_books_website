acf09e0e258b61f91db95f4d45bac9f88038ddf8
UMANGBHOTE <bhoteumang@gmail.com>
2025-12-10 16:08:53 +0900

    Add files via upload

---
+++ Fullstack-books-website/server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    res.json({
+        success: true,
+        filename: req.file.filename,
+        url: 'http://localhost:5000/uploads/' + req.file.filename
+    });
+});
+
+app.listen(5000, () => {
+    console.log('Server running on http://localhost:5000');
+});
